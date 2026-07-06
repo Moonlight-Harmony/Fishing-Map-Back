83c937ad172da52989c8d5ba1bbdb73aa51e0299
sk <gajeong01@gmail.com>
2026-07-06 17:08:54 +0900

    feat: User 엔티티 생성

--- Fishing-Map-Back/src/main/java/com/moonlightharmony/fishingmapback/FishingMapBackApplication.java
+++ Fishing-Map-Back/src/main/java/com/moonlightharmony/fishingmapback/FishingMapBackApplication.java
@@ -2,8 +2,10 @@ package com.moonlightharmony.fishingmapback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FishingMapBackApplication {
 
 	public static void main(String[] args) {
