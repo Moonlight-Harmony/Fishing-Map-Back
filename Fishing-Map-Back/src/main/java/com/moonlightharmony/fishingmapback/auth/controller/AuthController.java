247ae2253f70108aff828274aae4a4c0ae78daab
sk <gajeong01@gmail.com>
2026-07-06 22:46:44 +0900

    feat: AuthController 로그인 API 구현

---
+++ Fishing-Map-Back/src/main/java/com/moonlightharmony/fishingmapback/auth/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.moonlightharmony.fishingmapback.auth.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.moonlightharmony.fishingmapback.auth.dto.LoginRequest;
+import com.moonlightharmony.fishingmapback.auth.dto.LoginResponse;
+import com.moonlightharmony.fishingmapback.auth.service.AuthService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public LoginResponse login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
