7f2fd3cf72fc69d0c465d40723bccb9ccb4d24bc
sk <gajeong01@gmail.com>
2026-07-07 19:14:33 +0900

    fix: FishSpecies @NoArgsConstructor access value 누락 수정

--- Fishing-Map-Back/src/main/java/com/moonlightharmony/fishingmapback/fish_species/entity/FishSpecies.java
+++ Fishing-Map-Back/src/main/java/com/moonlightharmony/fishingmapback/fish_species/entity/FishSpecies.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "fish_species")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FishSpecies extends BaseTimeEntity {
     
     @Id
