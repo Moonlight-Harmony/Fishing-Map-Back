d795692446fbb14bfaa27b46fce3965640d0f4b3
sk <gajeong01@gmail.com>
2026-07-11 22:26:38 +0900

    feat: FishingRecordImage 레포지토리 생성

---
+++ Fishing-Map-Back/src/main/java/com/moonlightharmony/fishingmapback/fishing_record/repository/FishingRecordImageRepository.java
@@ -0,0 +1,14 @@
+package com.moonlightharmony.fishingmapback.fishing_record.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.moonlightharmony.fishingmapback.fishing_record.entity.FishingRecordImage;
+
+public interface FishingRecordImageRepository extends JpaRepository<FishingRecordImage, Long> {
+
+    List<FishingRecordImage> findByFishingRecordIdInAndDisplayOrder(List<Long> ids, int displayOrder);
+
+    List<FishingRecordImage> findByFishingRecordIdOrderByDisplayOrderAsc(Long fishingRecordId);
+}
